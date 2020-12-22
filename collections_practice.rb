26e4c4464494f384e4c36b4e2173ce29c26fcbfd
remote-torrent-6747 <abbyc@sky.com>
2020-12-22 19:57:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,2 +1,7 @@
-def sort_array_asc
+def sort_array_asc(numbers)
+  numbers.sort
 end
+
+#Build a method sort_array_asc that takes in an array of integers and
+#returns a copy of the array with the integers in ascending order.
+#25, 7, 1
