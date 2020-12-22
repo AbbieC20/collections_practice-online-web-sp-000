3035b9f67d17dca5353fe74b97af70bc98f144c2
remote-torrent-6747 <abbyc@sky.com>
2020-12-22 20:10:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,9 +4,5 @@ end
 
 
 def sort_array_desc(numbers)
-  return numbers.sort { |a, b| b <=> a } 
+  return numbers.sort { |a, b| b <=> a }
 end
-
-#should return an array sorted in descending order
-#returns a copy of the array with the integers in descending order
-#[25, 7, 14])).to eq([25, 14, 7])
