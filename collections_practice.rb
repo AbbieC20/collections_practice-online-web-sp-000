51ba74a6788c96bf0aae0adbf3e92255e960d9c3
remote-torrent-6747 <abbyc@sky.com>
2020-12-22 20:05:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,9 @@ def sort_array_asc(numbers)
   return numbers.sort
 end
 
-#Build a method sort_array_asc that takes in an array of integers and
-#returns a copy of the array with the integers in ascending order.
-#25, 7, 1
+
+def sort_array_desc(numbers)
+end 
+
+#should return an array sorted in descending order
+#returns a copy of the array with the integers in descending order
