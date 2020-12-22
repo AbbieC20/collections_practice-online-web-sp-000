6daa4bdc7388985930a2c996c5522cc557f3547d
remote-torrent-6747 <abbyc@sky.com>
2020-12-22 20:38:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,12 +24,11 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  counter = 0
-  while counter < array.length
-    yield array[counter]
-    counter += 1
+  updated_array = []
+  array.each do |name|
+    
   end
-  return array
+  return updated_array
 end
 #"blake", "ashley", "scott"])).to eq(["bl$ke", "as$ley", "sc$tt"
 #taking an array as an input, change the 3rd character of each element to a dollar sign.
