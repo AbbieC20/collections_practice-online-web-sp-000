0724b40ba2ef7f95e53bcc9fb74c8d60915ec8ba
remote-torrent-6747 <abbyc@sky.com>
2020-12-22 20:07:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,7 +4,8 @@ end
 
 
 def sort_array_desc(numbers)
-end 
+end
 
 #should return an array sorted in descending order
 #returns a copy of the array with the integers in descending order
+#[25, 7, 14])).to eq([25, 14, 7])
